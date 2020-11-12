d3eaec45459c5f9c85f870db92bafd2753fb0189
arturfil <arturfil@hotmail.com>
2020-11-12 03:03:44 -0500

    added relationships between activities and attendeedes

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -10,23 +11,26 @@ using Persistence;
 
 namespace Application.Activities
 {
-    public class List
+  public class List
+  {
+    public class Query : IRequest<List<ActivityDto>> { }
+
+    public class Handler : IRequestHandler<Query, List<ActivityDto>>
     {
-        public class Query : IRequest<List<Activity>> { }
+      private readonly DataContext _context;
+      private readonly IMapper _mapper;
+      public Handler(DataContext context, IMapper mapper)
+      {
+        this._mapper = mapper;
+        this._context = context;
+      }
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
-        {
-            private readonly DataContext _context;
-            public Handler(DataContext context)
-            {
-                this._context = context;
-            }
-
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
-            {
-                var activities = await _context.Activities.ToListAsync();
-                return activities;
-            }
-        }
+      public async Task<List<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
+      {
+        var activities = await _context.Activities
+          .ToListAsync();
+        return _mapper.Map<List<Activity>, List<ActivityDto>>(activities);
+      }
     }
+  }
 }
