2a462e7e0117e12c44ae45b533be85dfb0b8fb6d
arturfil <arturfil@hotmail.com>
2020-12-22 03:46:03 -0500

    added logic to filter and display Activities according to the filters selected

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Profiles;
 using MediatR;
@@ -15,6 +16,12 @@ namespace API.Controllers {
         [HttpPut]
         public async Task<ActionResult<Unit>> Edit(Edit.Command command) {
           return await Mediator.Send(command);
+        }
+
+        [HttpGet("{username}/activities")]
+        public async Task<ActionResult<List<UserActivityDto>>> GetUserActivities(string username,
+        string predicate) {
+          return await Mediator.Send(new ListActivities.Query{Username = username, Predicate = predicate});
         }
     }
 
